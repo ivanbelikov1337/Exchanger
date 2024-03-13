29fe2b18df171670166907c81102bc50622a296d
ivan <vanahilvnij343@gmail.com>
2024-03-13 14:17:33 +0200

    Line 13 add useCallback & Line 20 change useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
 import './App.css'
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 type Themes = "switch-on" |  null
 
 const App = () => {
     const [mode, setMode] = useState<Themes>(null)
-    const [finallySum, setFinallySum] = useState(0)
+    const [finallySum, setFinallySum] = useState<number>(0)
+    const [enterValue, setEnterValue] = useState<number>(0)
     const [currentPrice, setCurrentPrice] = useState<number>(0)
     const toggle = () => setMode((prev) => (prev === "switch-on" ? null : "switch-on"))
-    const price = { USD:currentPrice, ETN: "1"}
 
-
-    const calcSum = (val: number) => {
+    const calcSum = useCallback(() => {
         let sum = 0;
-        sum = val * +price["USD"]
-        setFinallySum(Math.ceil((sum / +price["ETN"]) * 100) / 100)
-    }
+        sum = enterValue * +currentPrice
+        setFinallySum(Math.ceil((sum) * 100) / 100)
+    }, [currentPrice, enterValue])
 
 
     useEffect(() => {
-        const socket = new WebSocket("wss://fstream.binance.com/ws/ethusdt@aggTrade")
-        socket.onmessage = event => setCurrentPrice(JSON.parse(event.data).p)
-    },[])
+        const socket = new WebSocket("wss://fstream.binance.com/ws/ethusdt@bookTicker")
+        socket.onmessage = event => {
+            !mode ?  setCurrentPrice(JSON.parse(event.data).a) : setCurrentPrice(JSON.parse(event.data).b)
+        }
+        calcSum()
+    },[calcSum,mode])
 
     return (
         <main className="wrapper">
-            <p className="currentPrice">Current price ETN: {currentPrice}</p>
+            <p className="currentPrice">Current price ETN: {currentPrice} $</p>
                 <form className="form">
                     <div className="box">
                         <label htmlFor="name" className="label">ETH amount:</label>
-                        <input onChange={(e) => calcSum(+e.target.value)}
+                        <input onChange={(e) => setEnterValue(+e.target.value)}
                                className="input"
                                type="number"
                         />
